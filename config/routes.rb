35511779bada0301532a64024b7364e657e87715
Ana Baptista <ana.bandarra@gmail.com>
2022-05-25 13:05:46 +0100

    bookings done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :grannies
-
+  resources :grannies do
+    resources :bookings, only: [ :new, :create ]
+  end
 end
