f7c9114aad93e9f3360d87ec0c8c8ec712721a14
Ana Baptista <ana.bandarra@gmail.com>
2022-05-26 18:22:09 +0100

    user profile done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   resources :grannies do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :users do
-    resources :bookings, only: :index
-  end
+  resources :bookings, only: :index
 end
