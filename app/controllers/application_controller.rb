9acd17dcb1fb4da120592d6dba458aad5b0acecb
Ana Baptista <ana.bandarra@gmail.com>
2022-05-26 14:54:08 +0100

    photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birth_date, :description, :granny, :location, :price])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birth_date, :description, :granny, :location, :price, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birth_date, :description, :granny, :location, :price])
   end
 end
